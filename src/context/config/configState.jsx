8d5b495046b7f40a0f6179d4f149605946ed4771
Tanmoy Sarkar <57363826+tanmoysrt@users.noreply.github.com>
2023-08-15 14:55:05 +0530

    use swiftwave proxy

--- src/context/config/configState.jsx
+++ src/context/config/configState.jsx
@@ -5,49 +5,53 @@ import ConfigContext from "./configContext";
 import axios from "axios";
 
 const ConfigState = (props) => {
-    const initialState =  localStorage.getItem("config") ? JSON.parse(localStorage.getItem("config")) : {
-        "server" : {
-            "host" : "",
-            "port" : ""
-        }
-    }
-    const [state, setState] = useState(initialState)
+  const initialState = localStorage.getItem("config")
+    ? JSON.parse(localStorage.getItem("config"))
+    : {
+        server: {
+          host: "",
+          port: "",
+        },
+      };
+  const [state, setState] = useState(initialState);
 
-    // Functions
+  // Functions
 
-    // Set the server host and port
-    const setServer = (host, port) => {
-        var newState = {
-            ...state,
-            "server" : {
-                "host" : host,
-                "port" : port
-            }
-        }
-        setState(newState)
-        localStorage.setItem("config", JSON.stringify(newState))
-        axios.defaults.baseURL = "http://"+host+":"+port
-    }
+  // Set the server host and port
+  const setServer = (host, port) => {
+    var newState = {
+      ...state,
+      server: {
+        host: host,
+        port: port,
+      },
+    };
+    setState(newState);
+    localStorage.setItem("config", JSON.stringify(newState));
 
-    // Check if the server host and port are set
-    const isSet = () => {
-        return state.server.host !== "" && state.server.port !== ""
-    }
+  };
 
-    // Get the server host and port
-    const getURI = () => {
-        return "http://"+state.server.host+":"+state.server.port 
-    }
+  // Check if the server host and port are set
+  const isSet = () => {
+    return state.server.host !== "" && state.server.port !== "";
+  };
 
-    // Set default axios config
-    axios.defaults.baseURL = getURI()
+  // Get the server host and port
+  const setURI = () => {
+    let baseURL = state.server.host + ":" + state.server.port;
+    axios.defaults.baseURL = "https://proxy.swiftwave.org";
+    axios.defaults.headers.common["X-PROXY-PASS"] = baseURL;
+  };
 
-    // Return
-    return (
-        <ConfigContext.Provider value={{state, setServer, isSet}}>
-            {props.children}
-        </ConfigContext.Provider>
-    )
-}
+  // Set default axios config
+  setURI()
+
+  // Return
+  return (
+    <ConfigContext.Provider value={{ state, setServer, isSet }}>
+      {props.children}
+    </ConfigContext.Provider>
+  );
+};
 
 export default ConfigState;
