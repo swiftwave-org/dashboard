ec258aa95e8be84dd02adea595e005a00e0e8646
Tanmoy Sarkar <57363826+tanmoysrt@users.noreply.github.com>
2024-02-11 19:18:26 +0530

    feat: udp ingress + secret text field added (#80)
    (cherry picked from commit 4a3ea7368e65337ccf1e6892ad1797d8e78a486e)

---
+++ src/views/components/SecuredText.vue
@@ -0,0 +1,21 @@
+<script setup>
+
+import { ref } from 'vue'
+
+let isVisible = ref(false)
+
+function toggleVisibility() {
+  isVisible.value = !isVisible.value
+}
+</script>
+
+<template>
+  <div class="text-sm text-gray-900 min-w-[100px]">
+    <span v-if="isVisible" class="flex items-center w-fit"><slot></slot><font-awesome-icon icon="fa-solid fa-eye" class="ml-2 cursor-pointer" :onclick="toggleVisibility" /></span>
+    <span v-else class="flex items-center w-fit">********<font-awesome-icon icon="fa-solid fa-eye-slash" class="ml-2 cursor-pointer" :onclick="toggleVisibility" /></span>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
--- src/views/pages/IngressRuleManagement.vue
+++ src/views/pages/IngressRuleManagement.vue
@@ -28,7 +28,7 @@ const newIngressRuleDetails = reactive({
   domainId: 0,
   port: 80,
   applicationId: '',
-  targetPort: 80,
+  targetPort: 80
 })
 
 const {
@@ -53,6 +53,11 @@ const {
 
 onIngressRuleCreateSuccess(() => {
   closeModal()
+  newIngressRuleDetails.protocol = 'http'
+  newIngressRuleDetails.domainId = 0
+  newIngressRuleDetails.port = 80
+  newIngressRuleDetails.applicationId = ''
+  newIngressRuleDetails.targetPort = 80
   toast.success('Ingress Rule created successfully')
   refetchIngressRules()
 })
@@ -62,11 +67,13 @@ onIngressRuleCreateFail((err) => {
 })
 
 const onChangeProtocol = () => {
-  if(newIngressRuleDetails.protocol === "https"){
-    newIngressRuleDetails.port = 443;
-  }
-  else if(newIngressRuleDetails.protocol === "http") {
-    newIngressRuleDetails.port = 80;
+  if (newIngressRuleDetails.protocol === 'https') {
+    newIngressRuleDetails.port = 443
+  } else if (newIngressRuleDetails.protocol === 'http') {
+    newIngressRuleDetails.port = 80
+  } else {
+    newIngressRuleDetails.port = 81
+    newIngressRuleDetails.domainId = 0
   }
 }
 
@@ -190,18 +197,27 @@ onIngressRulesError((err) => {
             <div class="mt-2 flex space-x-2">
               <select
                 v-model="newIngressRuleDetails.protocol"
-                @change="onChangeProtocol"
-                class="w-4/12 block rounded-md border-gray-300 shadow-sm focus:border-primary-500 focus:ring-primary-500 sm:text-sm">
+                class="w-4/12 block rounded-md border-gray-300 shadow-sm focus:border-primary-500 focus:ring-primary-500 sm:text-sm"
+                @change="onChangeProtocol">
                 <option value="http">HTTP</option>
                 <option value="https">HTTPS</option>
                 <option value="tcp">TCP</option>
+                <option value="udp">UDP</option>
               </select>
               <select
+                v-show="newIngressRuleDetails.protocol === 'http' || newIngressRuleDetails.protocol === 'https'"
                 v-model="newIngressRuleDetails.domainId"
-                class="block w-full rounded-md border-gray-300 shadow-sm focus:border-primary-500 focus:ring-primary-500 sm:text-sm">
+                class="block w-full rounded-md border-gray-300 shadow-sm focus:border-primary-500 focus:ring-primary-500 sm:text-sm"
+              >
                 <option value="0">Select a domain</option>
                 <option v-for="domain in domains" :key="domain.id" :value="domain.id">{{ domain.name }}</option>
               </select>
+              <p
+                v-show="newIngressRuleDetails.protocol === 'tcp' || newIngressRuleDetails.protocol === 'udp'"
+                class="text-base font-medium text-gray-700 w-full flex items-center justify-end pr-2"
+              >
+                Listen on port
+              </p>
               <input
                 v-model="newIngressRuleDetails.port"
                 :readonly="newIngressRuleDetails.protocol === 'https'"
@@ -210,6 +226,11 @@ onIngressRulesError((err) => {
                 placeholder="Port"
                 type="number" />
             </div>
+            <p
+              v-if="newIngressRuleDetails.protocol === 'tcp' || newIngressRuleDetails.protocol === 'udp'"
+              class="mt-2 text-sm text-danger-500"
+            ><b>NOTE: </b>You don't need to specify the domain for TCP and UDP protocols. While connecting to the
+              server, use the server IP address instead of the domain name.</p>
           </div>
 
           <div class="text-center w-full mt-4 text-xl">
@@ -223,7 +244,9 @@ onIngressRulesError((err) => {
                 v-model="newIngressRuleDetails.applicationId"
                 class="block w-full rounded-md border-gray-300 shadow-sm focus:border-primary-500 focus:ring-primary-500 sm:text-sm">
                 <option value="">Select application name</option>
-                <option v-for="application in applications" :key="application.id" :value="application.id">{{ application.name }}</option>
+                <option v-for="application in applications" :key="application.id" :value="application.id">
+                  {{ application.name }}
+                </option>
               </select>
               <input
                 v-model="newIngressRuleDetails.targetPort"
@@ -277,10 +300,18 @@ onIngressRulesError((err) => {
           </TableRow>
           <TableRow align="center">
             <div class="text-sm text-gray-900">
-              <a :href="ingressRule.protocol+'://' + ingressRule.domain.name + ':' + ingressRule.port.toString()" target="_blank"
+              <a v-if="ingressRule.protocol === 'http' || ingressRule.protocol === 'https'"
+                 :href="ingressRule.protocol+'://' + ingressRule.domain.name + ':' + ingressRule.port.toString()"
+                 target="_blank"
               >{{ ingressRule.protocol }}://{{ ingressRule.domain.name }}:{{ ingressRule.port }}</a
-              >&nbsp;&nbsp; <font-awesome-icon icon="fa-solid fa-arrow-right" />&nbsp;&nbsp;
-              <a target="_blank">{{ ingressRule.application.name }}:{{ ingressRule.targetPort }}</a>
+              >
+              <a v-else-if="ingressRule.protocol === 'tcp'"
+                 href="javascript:void(0);">tcp://&lt;server-ip&gt;:{{ ingressRule.port }}</a>
+              <a v-else-if="ingressRule.protocol === 'udp'"
+                 href="javascript:void(0);">udp://&lt;server-ip&gt;:{{ ingressRule.port }}</a>
+              <a v-else href="javascript:void(0);"><i>Unknown</i></a>
+              &nbsp;&nbsp;<font-awesome-icon icon="fa-solid fa-arrow-right" />&nbsp;&nbsp;
+              <a href="javascript:void(0);">{{ ingressRule.application.name }}:{{ ingressRule.targetPort }}</a>
             </div>
           </TableRow>
           <TableRow align="right">
