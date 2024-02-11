f6db668ad6f9bf8069987b65c912f587bbebe769
Tanmoy Sarkar <57363826+tanmoysrt@users.noreply.github.com>
2024-02-11 17:00:10 +0530

    feat: added udp support

--- src/views/pages/IngressRuleManagement.vue
+++ src/views/pages/IngressRuleManagement.vue
@@ -53,6 +53,11 @@ const {
 
 onIngressRuleCreateSuccess(() => {
   closeModal()
+  newIngressRuleDetails.protocol = 'http'
+  newIngressRuleDetails.domainId = 0
+  newIngressRuleDetails.port = 80
+  newIngressRuleDetails.applicationId = ''
+  newIngressRuleDetails.targetPort = 80
   toast.success('Ingress Rule created successfully')
   refetchIngressRules()
 })
@@ -66,6 +71,9 @@ const onChangeProtocol = () => {
     newIngressRuleDetails.port = 443
   } else if (newIngressRuleDetails.protocol === 'http') {
     newIngressRuleDetails.port = 80
+  } else {
+    newIngressRuleDetails.port = 81
+    newIngressRuleDetails.domainId = 0
   }
 }
 
@@ -194,13 +202,22 @@ onIngressRulesError((err) => {
                 <option value="http">HTTP</option>
                 <option value="https">HTTPS</option>
                 <option value="tcp">TCP</option>
+                <option value="udp">UDP</option>
               </select>
               <select
+                v-show="newIngressRuleDetails.protocol === 'http' || newIngressRuleDetails.protocol === 'https'"
                 v-model="newIngressRuleDetails.domainId"
-                class="block w-full rounded-md border-gray-300 shadow-sm focus:border-primary-500 focus:ring-primary-500 sm:text-sm">
+                class="block w-full rounded-md border-gray-300 shadow-sm focus:border-primary-500 focus:ring-primary-500 sm:text-sm"
+              >
                 <option value="0">Select a domain</option>
                 <option v-for="domain in domains" :key="domain.id" :value="domain.id">{{ domain.name }}</option>
               </select>
+              <p
+                v-show="newIngressRuleDetails.protocol === 'tcp' || newIngressRuleDetails.protocol === 'udp'"
+                class="text-base font-medium text-gray-700 w-full flex items-center justify-end pr-2"
+              >
+                Listen on port
+              </p>
               <input
                 v-model="newIngressRuleDetails.port"
                 :readonly="newIngressRuleDetails.protocol === 'https'"
@@ -209,6 +226,11 @@ onIngressRulesError((err) => {
                 placeholder="Port"
                 type="number" />
             </div>
+            <p
+              v-if="newIngressRuleDetails.protocol === 'tcp' || newIngressRuleDetails.protocol === 'udp'"
+              class="mt-2 text-sm text-danger-500"
+            ><b>NOTE: </b>You don't need to specify the domain for TCP and UDP protocols. While connecting to the
+              server, use the server IP address instead of the domain name.</p>
           </div>
 
           <div class="text-center w-full mt-4 text-xl">
