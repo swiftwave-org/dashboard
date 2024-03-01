891c739da065ff95c9205d213d49ee044ca4566a
Tanmoy Sarkar <57363826+tanmoysrt@users.noreply.github.com>
2024-03-01 18:06:53 +0530

    feat: added markdown renderer

---
+++ src/views/components/MarkdownRenderer.vue
@@ -0,0 +1,30 @@
+<template>
+  <div v-html="markdown.render(source)" />
+</template>
+
+<script setup>
+import MarkdownIt from 'markdown-it'
+import MarkdownItAbbr from 'markdown-it-abbr'
+import MarkdownItAnchor from 'markdown-it-anchor'
+import MarkdownItFootnote from 'markdown-it-footnote'
+import MarkdownItHighlightjs from 'markdown-it-highlightjs'
+import MarkdownItSub from 'markdown-it-sub'
+import MarkdownItSup from 'markdown-it-sup'
+import MarkdownItTasklists from 'markdown-it-task-lists'
+
+const markdown = new MarkdownIt()
+  .use(MarkdownItAbbr)
+  .use(MarkdownItAnchor)
+  .use(MarkdownItFootnote)
+  .use(MarkdownItHighlightjs)
+  .use(MarkdownItSub)
+  .use(MarkdownItSup)
+  .use(MarkdownItTasklists)
+
+defineProps({
+  source: {
+    type: String,
+    default: ''
+  }
+})
+</script>
