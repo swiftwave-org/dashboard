0d59a43b1b62def74f72b606052a4ab3f77003eb
Tanmoy Sarkar <57363826+tanmoysrt@users.noreply.github.com>
2024-03-03 03:31:12 +0530

    chore: added custom scrollbar

--- src/views/components/Table/Table.vue
+++ src/views/components/Table/Table.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="overflow-hidden border border-gray-200 md:rounded-lg">
+  <div class="scrollbox max-h-[80vh] overflow-y-auto border border-gray-200 md:rounded-lg">
     <table class="min-w-full divide-y divide-gray-200">
       <thead class="bg-gray-50">
         <tr>
@@ -13,3 +13,17 @@
     <slot name="message"></slot>
   </div>
 </template>
+
+<style scoped>
+.scrollbox::-webkit-scrollbar {
+  width: 9px;
+}
+
+.scrollbox::-webkit-scrollbar-track {
+  @apply rounded-full bg-gray-200;
+}
+
+.scrollbox::-webkit-scrollbar-thumb {
+  @apply rounded-full bg-primary-500;
+}
+</style>
